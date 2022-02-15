31f017ef7e148a830d45ff0ccceeb09e069d9ebc
mehakisrar <mehakisrar83@gmail.com>
2022-02-15 16:48:32 +0500

    Customizing view folder bascially checking if we change the name of default view folder name and rename the folder how it will work

--- serverstaticwebsite/index.js
+++ serverstaticwebsite/index.js
@@ -7,13 +7,23 @@ const port = 3000;
 console.log(path.join(__dirname, '../serverstaticwesbite/src')); //to move to the next path 
 const staticpath = (path.join(__dirname, "../serverstaticwesbite/src"));
 
+const viewchange = (path.join(__dirname, "../changeview"));
+
+
 app.set("view engine", "hbs");
+app.set("views", viewchange);
 app.use(express.static(staticpath));
+
 
 app.get("", (req, res) => {
     res.render('index', {
         changing: "Developmentfeature"
     });
+});
+
+app.get("/about", (req, res) => {
+
+    res.render("about");
 });
 app.get('/', (req, res) => {
     res.send("Hi I am express website home page");
